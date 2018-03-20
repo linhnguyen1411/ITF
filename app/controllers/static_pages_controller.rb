9121bded4f81dfb497ddb4de89fe08ba1e7d9f8a
danh13t1 <danh13t1@gmail.com>
2018-03-20 10:56:25 +0700

    add-folder-template

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,14 @@
 class StaticPagesController < ApplicationController
   def index
   end
-  def question_detial
+  def question_detail
+  end
+  def question_list
+  end
+  def profile
+  end
+  def login
+  end
+  def register
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
-  get 'static_pages/question_detial'
+  get 'static_pages/question_detail'
+  get 'static_pages/question_list'
+  get 'static_pages/profile'
+  get 'static_pages/login'
+  get 'static_pages/register'
 end
