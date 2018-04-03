0fa09e36789ec3f35bb8bdc2ed7d45b0e9ca6741
dbg1995 <vo.van.danh@b121254-mb.local>
2018-04-03 09:58:02 +0700

    create-post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,35 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.new post_params
+    @post.user_id = current_user.id
+    Post.transaction do
+	  @post.save!
+	  save_post_tags(@post, params[:tags]) if params[:tags].present?
+	  flash[:success] = t "flash.create.create_success"
+      respond_to do |format|
+	    format.js
+	  end
+	end
+  rescue
+    flash[:danger] = t "flash.create.create_failure"
+    respond_to do |format|
+	  format.js
+	end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit :type, :title, :content
+  end
+
+  def save_post_tags post, tag
+  	params[:tags].split(",").each do |item|
+      tag = Tag.find_or_create_by! name: item
+      PostTag.create post_id: post.id, tag_id: tag.id
+    end
+  end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,7 +2,10 @@ class Tag < ApplicationRecord
   has_many :post_tags, dependent: :destroy
   has_many :posts, through: :post_tags
 
+  validates :name, presence: true, length: {maximum: Settings.tag.max_name,
+    minimum: Settings.tag.min_name}
+
   scope :order_by_used_count, -> do
-    joins(:post_tags).group("tags.id").order("count(tags.id) desc")
+    left_joins(:post_tags).group("tags.id").order("count(post_tags.id) desc")
   end
 end
