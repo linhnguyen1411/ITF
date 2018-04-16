4a942f75f2065a1726b04075c2ba9fbee2e05841
danh13t1 <danh13t1@gmail.com>
2018-04-16 23:22:07 +0700

    series-model

--- app/assets/javascripts/series.js
+++ app/assets/javascripts/series.js
@@ -1,7 +1,34 @@
 $(document).ready(function(){
   $(document).on('click','.add-post',function(){
-    var post_id = $(this).data('id');
-    $(this).closest('.media-group').addClass('selected');
-    $('.btn-accept-add-posts').data('post-ids') = $('.btn-accept-add-posts').data('post-ids') + post_id
+    if($(this).closest('.media-group').hasClass('selected')){
+      $(this).closest('.media-group').removeClass('selected');
+      var post_id = $(this).data('id');
+      var index = $('#post_list_modal').data('post-ids').indexOf(post_id);
+      if (index > -1) {
+        $('#post_list_modal').data('post-ids').splice(index, 1);
+      }
+    }else {
+      var post_id = $(this).data('id');
+      $(this).closest('.media-group').addClass('selected');
+      $('#post_list_modal').data('post-ids').push(post_id);
+    }
   });
+
+  $(document).on('click','.btn-accept-add-posts',function(){
+    var post_ids =  $('#post_list_modal').data('post-ids').join(',');
+    $.ajax({
+      url: window.location.pathname  + '?post_ids=' + post_ids,
+      type: 'PUT',
+      dataType: 'html',
+      success: function (data) {
+        window.location.replace(window.location.pathname);
+      },
+      error: function () {
+        response([]);
+      }
+    });
+  });
+  $('#post_list_modal').on('hidden.bs.modal', function () {
+    $('#post_list_modal').data('post-ids', $('#post_list_modal').data('current'));
+  })
 });
--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -1,9 +1,11 @@
 class SeriesController < ApplicationController
-  before_action :load_series, only: [:show]
+  before_action :load_series, only: [:show, :update]
 
   def index
     @type = Series.name
-    @series = Series.page(params[:page]).per Settings.paginate_default
+    @series = Series.page(params[:page]).per Settings.paginate_series
+    @popular_tags = Tag.order_by.posts_count.include_posts_count.limit Settings.paginate_default
+    @top_users = User.order_by_point.limit Settings.paginate_little
   end
 
   def new
@@ -12,8 +14,10 @@ class SeriesController < ApplicationController
 
   def show
     @votes = @series.reactions.include_user
-    @posts = @series.posts
+    @posts = @series.posts.includes_full
     @replies = Reply.by_replyable(@series).includes_full
+    @selected_post_id_list = current_user.posts.article.not_belong_to_series(@series.id)
+      .select{|post| post.series_id != nil }.pluck(:id)
   end
 
   def create
@@ -23,6 +27,27 @@ class SeriesController < ApplicationController
     else
     end
   end
+
+  def update
+    Post.transaction do
+      post_ids = current_user.posts.article.not_belong_to_series(@series.id).select{|post| post.series_id != nil }.pluck(:id).map(&:to_s)
+      add_post_ids =  params[:post_ids].split(',') - post_ids
+      remove_post_ids = post_ids - params[:post_ids].split(',')
+      add_post_ids.each do |post_id|
+        post = Post.article.find_by id: post_id
+        post.update_attributes! series_id: @series.id
+      end
+      remove_post_ids.each do |post_id|
+        post = Post.article.find_by id: post_id
+        post.update_attributes! series_id: nil
+      end
+    end
+    flash[:success] = t ".flash_success.add_posts"
+  rescue
+    flash[:danger] = t ".flash_danger.add_posts"
+  end
+
+  private
 
   def series_params
     params.require(:series).permit :title, :content
